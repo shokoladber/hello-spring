3140c44193315c9e9958e6e6729ad8577b77572d
shokoladber <allen.r.michael@gmail.com>
2022-02-07 19:11:47 -0600

    10.5. Exercises: Controllers and Routing

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @ResponseBody
-@RequestMapping("hello")
+@RequestMapping(value="hello")
 public class HelloController {
 
 //    @GetMapping("hello")
@@ -14,34 +14,66 @@ public class HelloController {
 //        return "Hello, Spring!";
 //    }
 
-    @GetMapping("goodbye")
-    public String goodbye(){
-        return "Goodbye, Spring!";
-    }
+//    @GetMapping("goodbye")
+//    public String goodbye(){
+//        return "Goodbye, Spring!";
+//    }
 
     // create handler that handles request of form /hello?name=LaunchCode
-    @RequestMapping(method={RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name){
-        return "Hello, " + name + "!";
-
+    @GetMapping
+    public String helloWithQueryParam(@RequestParam String name, @RequestParam String language){
+        String greeting = "";
+        if (language.equals("english")) {
+            greeting = "Hello, " + name + "!";
+        } else if (language.equals("italian")) {
+            greeting = "Buongiorno, " + name + "!";
+        } else if (language.equals("french")) {
+            greeting = "Bonjour, " + name + "!";
+        }
+        return greeting;
     }
 
-    //Handle requests of the form /hello/LaunchCode
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name, @PathVariable String friend){
-        return "Hello, " + name + "!";
-    }
+//    //Handle requests of the form /hello/LaunchCode
+//    @RequestMapping(method=RequestMethod.GET)
+//    public String helloWithPathParam(@RequestParam String name, @RequestParam String friend, @RequestParam String language) {
+//
+//        String greeting = "";
+//
+//        if (name == "") {
+//            return "World";
+//        } else if (language.equals("english")) {
+//            greeting = "Hello, " + name + "!";
+//        } else if (language.equals("italian")) {
+//            greeting = "Buongiorno, " + name + "!";
+//        } else if (language.equals("french")) {
+//            greeting = "Bonjour, " + name + "!";
+//        } else if (language.equals("spanish")) {
+//            greeting = "Hola, " + name + "!";
+//        } else if (language.equals("german")) {
+//            greeting = "Gutentag, " + name + "!";
+//        }
+//        return greeting;
+//
+//    }
 
-    @GetMapping("form")
+    @RequestMapping(value="form", method=RequestMethod.GET)
     public String helloForm(){
         return "<html>" +
-                "<body>" +
-                "<form action='/hello' method='post'>" +  //submit a request to /hello
-                "<input type='text' name='name'>" +
-                "<input type='submit' value='Greet me!'>" +
-                "<input type='submit' value='Friend'>" +
-                "</form>" +
-                "</body>" +
+                    "<body>" +
+                        "<form action='/hello' method='get'>" +  //submit a request to /hello
+                            "<input type='text' name='name'>" +
+                            "<select name='language'>" +
+                                "<option value=''>--Please choose an option--</option>" +
+                                "<option value='english'>English</option>" +
+                                "<option value='italian'>Italian</option>" +
+                                "<option value='french'>French</option>" +
+                                "<option value='spanish'>Spanish</option>" +
+                                "<option value='german'>German</option>" +
+                            "</select>" +
+                            "<input type='submit' value='Greet me!'>" +
+                            "<input type='submit' value='Friend'>" +
+                        "</form>" +
+                    "</body>" +
                 "</html>";
 
     }
